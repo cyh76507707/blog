425f97fac99ffce3d5750c983d64cfe613df2850
Andrew Cho <cyh76507707@gmail.com>
2015-12-02 06:51:00 +0000

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+    def home
+    end
+    
+    def about
+    end
+    
+    def menu
+    end
+    
+    def beans
+    end
+    
+    def blog
+    end
+
+end
